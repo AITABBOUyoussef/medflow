8e78803a7522e98694aad84c391a10a12829a36d
outamghartAchraf <outamghart2000@gmail.com>
2026-06-02 11:41:32 +0100

    add function loginAction for show page login

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once 'src/controller/UserController.php';
+
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
 
--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -3,4 +3,8 @@
 class UserController
 {
 
+    public static function loginAction()
+    {
+         require_once __DIR__ . "/../../views/auth/login.php";
+    }
 }
