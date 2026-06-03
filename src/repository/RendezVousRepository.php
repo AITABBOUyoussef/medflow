0118ab72059d2465c98268c200af56fddee3fa88
outamghartAchraf <outamghart2000@gmail.com>
2026-06-03 11:46:54 +0100

    add function findAll rendevous

--- src/repository/RendezVousRepository.php
+++ src/repository/RendezVousRepository.php
@@ -1,1 +1,28 @@
+<?php
+
+include_once __DIR__ . "/../../config/DB.php";
+
+class RendezVousRepository
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = DB::connect();
+    }
+
+    public function findAll()
+    {
+        $stmt = $this->pdo->prepare("
+            SELECT *
+            FROM rendez_vous
+            ORDER BY date_rdv DESC
+        ");
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
  
+}
