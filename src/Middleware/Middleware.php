87dd25b4bf2958781198afa0fb9088933237b512
outamghartAchraf <outamghart2000@gmail.com>
2026-06-04 10:11:43 +0100

    add functions in meddelware for check role

--- src/Middleware/Middleware.php
+++ src/Middleware/Middleware.php
@@ -12,5 +12,33 @@ class Middleware
         }
     }
 
-   
+    public static function medecin()
+    {
+        self::auth();
+
+        if ($_SESSION['user']['role'] !== 'MEDECIN') {
+           header('Location: index.php?action=login');
+            exit;
+        }
+    }
+
+    public static function patient()
+    {
+        self::auth();
+
+        if ($_SESSION['user']['role'] !== 'PATIENT') {
+            header('Location: index.php?action=login');
+            exit;
+        }
+    }
+
+    public static function admin()
+    {
+        self::auth();
+
+        if ($_SESSION['user']['role'] !== 'ADMIN') {
+              header('Location: index.php?action=login');
+                exit;
+        }
+    }
 }
